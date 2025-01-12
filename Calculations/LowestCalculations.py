0fea568878d3749e1fa788d0a38e5a2ef8267b4f
Arman Behera <73575356+ArmanBehera@users.noreply.github.com>
2025-01-12 15:00:43 +0530

    Added for Calculations

---
+++ Calculations/LowestCalculations.py
@@ -0,0 +1,12 @@
+#lowest_melbourne = 143979.8173
+lowest_california = 0.4377720928967723
+
+while True:
+    #print("Fitness in Melbourne: ")
+    #fitness_melbourne = float(input())
+
+    print("Fitness in California: ")
+    fitness_california = float(input())
+
+    #print(f"Relative fitness for Melbourne: {lowest_melbourne / fitness_melbourne}")
+    print(f"Relative fitness for California: {lowest_california / fitness_california}")
